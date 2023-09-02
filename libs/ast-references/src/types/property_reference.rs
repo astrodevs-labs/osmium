2ee8dd772355b88555c7e69133fccbbc26318b25
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2023-09-02 12:13:08 -0400

    feat(libs/ast-references): property type is now a comprehensive enum

--- libs/ast-references/src/types/property_reference.rs
+++ libs/ast-references/src/types/property_reference.rs
@@ -1,12 +1,14 @@
-use std::cell::RefCell;
 /**
  * PropertyReference.rs
  * Definition of PropertyReference struct
  * author: 0xMemoryGrinder
-*/
+ */
 
 use std::fmt;
 use std::rc::Rc;
+use std::cell::RefCell;
+use syn_solidity::Type;
+
 use crate::types::location::Location;
 use crate::types::contract_reference::ContractReference;
 
@@ -17,7 +19,7 @@ use crate::types::contract_reference::ContractReference;
 
 pub struct PropertyReference {
     pub name: String,
-    pub prop_type: String,
+    pub ty: Type,
     pub location: Location,
     pub contract: Rc<RefCell<ContractReference>>,
 }
@@ -27,10 +29,10 @@ pub struct PropertyReference {
  *****************************************************************************/
 
  impl PropertyReference {
-    pub fn new(name: String, prop_type: String, location: Location, contract: &Rc<RefCell<ContractReference>>) -> PropertyReference {
+    pub fn new(name: String, ty: Type, location: Location, contract: &Rc<RefCell<ContractReference>>) -> PropertyReference {
         PropertyReference {
             name: name,
-            prop_type,
+            ty,
             location: location,
             contract: contract.clone(),
         }
@@ -62,6 +64,8 @@ impl PartialEq for PropertyReference {
  #[cfg(test)]
  mod tests {
     use std::cell::RefCell; 
+    use proc_macro2::Span;
+
     use crate::types::{location::{Bound, Location}, file_reference::FileReference};
 
     use super::*;
@@ -70,9 +74,9 @@ impl PartialEq for PropertyReference {
     fn new_good_construct() {
         let file = Rc::new(RefCell::new(FileReference::new("test.sol".to_string())));
         let contract = Rc::new(RefCell::new(ContractReference::new("Test".to_string(), Location::new("test.sol".to_string(), Bound::new(0, 0), Bound::new(0, 0)), &file)));
-        let property = PropertyReference::new("test".to_string(), "uint256".to_string(), Location::new("test.sol".to_string(), Bound::new(0, 0), Bound::new(0, 0)), &contract);
+        let property = PropertyReference::new("test".to_string(), Type::Bool(Span::call_site()), Location::new("test.sol".to_string(), Bound::new(0, 0), Bound::new(0, 0)), &contract);
         assert_eq!(property.name, "test");
-        assert_eq!(property.prop_type, "uint256");
+        assert_eq!(property.ty, Type::Bool(Span::call_site()));
         assert_eq!(property.location, Location::new("test.sol".to_string(), Bound::new(0, 0), Bound::new(0, 0)));
         assert_eq!(property.contract, contract);
     }

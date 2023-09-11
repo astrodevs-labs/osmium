568c59a8820bd947389877c9790fcef522da070b
Louis PIOCHAUD <lpiochaud@sibylone.com>
2023-09-10 21:29:32 -0700

    [#51] tests: using.rs

--- libs/ast-extractor/src/retriever/using.rs
+++ libs/ast-extractor/src/retriever/using.rs
@@ -27,3 +27,55 @@ pub fn retrieve_usings_nodes(ast: syn_solidity::ItemContract) -> Vec<ItemUsing> 
     visitor.visit_item_contract(&ast);
     visitor.usings
 }
+
+
+#[cfg(test)]
+mod tests {
+    use proc_macro2::TokenStream;
+    use syn_solidity::Item;
+
+    use super::*;
+    use std::fs;
+    use std::path::PathBuf;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_retrieve_using_nodes_empty() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("tests");
+        path.push("files");
+        path.push("using");
+        path.push("empty.sol");
+        let source = fs::read_to_string(path).unwrap();
+        let tokens = TokenStream::from_str(source.as_str()).unwrap();
+        let ast = syn_solidity::parse2(tokens).unwrap();
+        let item = ast.items.first().unwrap().clone();
+
+        if let Item::Contract(contract) = item {
+            let res = retrieve_usings_nodes(contract);
+            assert_eq!(res.len(), 0);
+        } else {
+            panic!("Item should not have any using directive");
+        }
+    }
+
+    #[test]
+    fn test_retrieve_using_nodes_one() {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("tests");
+        path.push("files");
+        path.push("using");
+        path.push("one.sol");
+        let source = fs::read_to_string(path).unwrap();
+        let tokens = TokenStream::from_str(source.as_str()).unwrap();
+        let ast = syn_solidity::parse2(tokens).unwrap();
+        let item = ast.items.first().unwrap().clone();
+
+        if let Item::Contract(contract) = item {
+            let res = retrieve_usings_nodes(contract);
+            assert_eq!(res.len(), 1);
+        } else {
+            panic!("Item should have a a using directive");
+        }
+    }
+}

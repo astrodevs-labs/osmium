92d4ffc5094683e2a5c65a7c3c4d005da68dd380
0xtekgrinder <0xtekgrinder@protonmail.com>
2023-09-15 00:14:06 -0400

    refactor(solidhunter): contract_name_pascalcase use ast_extractor

--- toolchains/solidity/linter/core/solidhunter-lib/src/rules/naming/contract_name_pascalcase.rs
+++ toolchains/solidity/linter/core/solidhunter-lib/src/rules/naming/contract_name_pascalcase.rs
@@ -1,28 +1,22 @@
+use ast_extractor::Spanned;
+
 use crate::linter::SolidFile;
 use crate::rules::types::*;
 use crate::types::*;
-use solc_wrapper::{decode_location, CodeLocation, SourceUnitChildNodes};
+
+pub const RULE_ID: &str = "contract-name-pascalcase";
+const MESSAGE: &str = "Contract name need to be in pascal case";
 
 pub struct ContractNamePascalCase {
     data: RuleEntry,
 }
 
 impl ContractNamePascalCase {
-    fn create_diag(&self, location: (CodeLocation, CodeLocation), file: &SolidFile) -> LintDiag {
+    fn create_diag(&self, location: Range, file: &SolidFile) -> LintDiag {
         LintDiag {
-            id: "contract-name-pascalcase".to_string(),
-            range: Range {
-                start: Position {
-                    line: location.0.line as u64,
-                    character: location.0.column as u64,
-                },
-                end: Position {
-                    line: location.1.line as u64,
-                    character: location.1.column as u64,
-                },
-                length: location.0.length as u64,
-            },
-            message: "Contract name need to be in pascal case".to_string(),
+            id: RULE_ID.to_string(),
+            range: location,
+            message: MESSAGE.to_string(),
             severity: Some(self.data.severity),
             code: None,
             source: None,
@@ -35,27 +29,28 @@ impl ContractNamePascalCase {
 impl RuleType for ContractNamePascalCase {
     fn diagnose(&self, file: &SolidFile, _files: &Vec<SolidFile>) -> Vec<LintDiag> {
         let mut res = Vec::new();
+        let contracts = ast_extractor::retriever::retrieve_contract_nodes(&file.data);
 
-        for node in &file.data.nodes {
-            match node {
-                SourceUnitChildNodes::ContractDefinition(contract) => {
-                    if (contract.name.chars().nth(0).unwrap() >= 'a'
-                        && contract.name.chars().nth(0).unwrap() <= 'z')
-                        || contract.name.contains('_')
-                        || contract.name.contains('-')
+        for contract in contracts {
+            if (contract.name.as_string().chars().nth(0).unwrap() >= 'a'
+                        && contract.name.as_string().chars().nth(0).unwrap() <= 'z')
+                        || contract.name.as_string().contains('_')
+                        || contract.name.as_string().contains('-')
                     {
-                        //Untested
-                        let location = decode_location(
-                            contract.name_location.as_ref().unwrap(),
-                            &file.content,
-                        );
-                        res.push(self.create_diag(location, file));
+                        res.push(self.create_diag({
+                            let location = contract.name.span();
+                            Range {
+                                start: Position {
+                                    line: location.start().line as u64,
+                                    character: location.start().column as u64,
+                                },
+                                end: Position {
+                                    line: location.end().line as u64,
+                                    character: location.end().column as u64,
+                                },
+                            }
+                        }, file));
                     }
-                }
-                _ => {
-                    continue;
-                }
-            }
         }
         res
     }
@@ -69,7 +64,7 @@ impl ContractNamePascalCase {
 
     pub(crate) fn create_default() -> RuleEntry {
         RuleEntry {
-            id: "contract-name-pascalcase".to_string(),
+            id: RULE_ID.to_string(),
             severity: Severity::WARNING,
             data: vec![],
         }

71edeeae17cad73348f3e28c09a483e1dc5bb45e
Théo Maringer <theo.maringer@epitech.eu>
2023-09-19 14:31:20 +0100

    feat(libs/ast-references): add visit variable definition

--- libs/ast-references/src/file_retriever.rs
+++ libs/ast-references/src/file_retriever.rs
@@ -8,13 +8,14 @@ use crate::types::event_reference::EventReference;
 use crate::types::file_reference::FileReference;
 use crate::types::struct_reference::StructReference;
 use crate::types::function_reference::FunctionReference;
+use crate::types::variable_reference::VariableReference;
 use crate::types::location::{Bound, Location};
 use proc_macro2::TokenStream;
 use std::cell::RefCell;
 use std::fs;
 use std::rc::Rc;
 use std::str::FromStr;
-use syn_solidity::{ItemContract, ItemEvent, ItemFunction, ItemStruct, Spanned, Visit};
+use syn_solidity::{ItemContract, ItemEvent, ItemFunction, ItemStruct, Spanned, VariableDeclaration, Visit};
 
 struct FileVisitor {
     file_reference: Rc<RefCell<FileReference>>,
@@ -31,6 +32,61 @@ impl FileVisitor {
 }
 
 impl<'ast> Visit<'ast> for FileVisitor {
+    fn visit_variable_declaration(&mut self, i: &'ast VariableDeclaration) {
+        if i.name.is_none() {
+            return;
+        }
+        let variable_reference = VariableReference::new(
+            i.name.as_ref().unwrap().0.to_string().clone(),
+            i.ty.clone(),
+            Location::new(
+                self.file_reference.borrow_mut().path.clone(),
+                Bound::new(
+                    i.name.span().start().line as u32,
+                    i.name.span().start().column as u32,
+                ),
+                Bound::new(
+                    i.name.span().end().line as u32,
+                    i.name.span().end().column as u32,
+                ),
+            ),
+            self.current_contract.as_ref(),
+            Some(&self.file_reference),
+        );
+        self.file_reference.borrow_mut().add_variable(variable_reference);
+        syn_solidity::visit::visit_variable_declaration(self, i)
+    }
+
+    // TODO: Add enum reference in contract references
+    //fn visit_item_enum(&mut self, i: &'ast ItemEnum) {
+    //    let enum_reference = EnumReference::new(
+    //        i.name.to_string(),
+    //        Location::new(
+    //            self.file_reference.borrow_mut().path.clone(),
+    //            Bound::new(
+    //                i.name.span().start().line as u32,
+    //                i.name.span().start().column as u32,
+    //            ),
+    //            Bound::new(
+    //                i.name.span().start().line as u32,
+    //                i.name.span().start().column as u32,
+    //            ),
+    //        ),
+    //        self.current_contract.as_ref(),
+    //        Some(&self.file_reference),
+    //    );
+    //    if self.current_contract.is_some() {
+    //        self.current_contract
+    //            .as_ref()
+    //            .unwrap()
+    //            .borrow_mut()
+    //            .add_enum(&Rc::new(RefCell::new(enum_reference)));
+    //    } else {
+    //        self.file_reference.borrow_mut().add_enum(enum_reference);
+    //    }
+    //    syn_solidity::visit::visit_item_enum(self, i)
+    //}
+
     fn visit_item_contract(&mut self, i: &ItemContract) {
         let contract_reference = ContractReference::new(
             i.name.to_string(),
@@ -61,35 +117,6 @@ impl<'ast> Visit<'ast> for FileVisitor {
         syn_solidity::visit::visit_item_contract(self, i);
         self.current_contract = None;
     }
-
-    //fn visit_item_enum(&mut self, i: &'ast ItemEnum) {
-    //    let enum_reference = EnumReference::new(
-    //        i.name.to_string(),
-    //        Location::new(
-    //            self.file_reference.borrow_mut().path.clone(),
-    //            Bound::new(
-    //                i.name.span().start().line as u32,
-    //                i.name.span().start().column as u32,
-    //            ),
-    //            Bound::new(
-    //                i.name.span().start().line as u32,
-    //                i.name.span().start().column as u32,
-    //            ),
-    //        ),
-    //        self.current_contract.as_ref(),
-    //        Some(&self.file_reference),
-    //    );
-    //    if self.current_contract.is_some() {
-    //        self.current_contract
-    //            .as_ref()
-    //            .unwrap()
-    //            .borrow_mut()
-    //            .add_enum(&Rc::new(RefCell::new(enum_reference)));
-    //    } else {
-    //        self.file_reference.borrow_mut().add_enum(enum_reference);
-    //    }
-    //    syn_solidity::visit::visit_item_enum(self, i)
-    //}
 
     fn visit_item_event(&mut self, i: &'ast ItemEvent) {
         let event_reference = EventReference::new(
@@ -138,9 +165,9 @@ impl<'ast> Visit<'ast> for FileVisitor {
                 ),
                 self.current_contract.as_ref().unwrap(),
             );
-            self.current_contract.as_ref().unwrap().borrow_mut().add_function(&Rc::new(RefCell::new(function_reference)))
+            self.current_contract.as_ref().unwrap().borrow_mut().add_function(&Rc::new(RefCell::new(function_reference)));
         }
-        syn_solidity::visit::visit_item_function(self, i)
+        syn_solidity::visit::visit_item_function(self, i);
     }
 
     fn visit_item_struct(&mut self, i: &'ast ItemStruct) {

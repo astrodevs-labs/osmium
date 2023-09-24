269e6abbc3a5642b218b8ba1a5fa95a96091c768
Théo Maringer <theo.maringer@epitech.eu>
2023-09-24 01:04:03 +0100

    feat(solidity/linter/core): EventNameCamelCase rule

--- toolchains/solidity/linter/core/solidhunter-lib/src/rules/naming/mod.rs
+++ toolchains/solidity/linter/core/solidhunter-lib/src/rules/naming/mod.rs
@@ -1,17 +1,19 @@
 use crate::rules::naming::contract_name_pascalcase::ContractNamePascalCase;
 use crate::rules::naming::func_name_camelcase::FuncNameCamelCase;
-
+use crate::rules::naming::event_name_camelcase::EventNameCamelCase;
 use crate::rules::naming::func_param_name_camelcase::FuncParamNameCamelcase;
 use crate::rules::naming::use_forbidden_name::UseForbiddenName;
 use crate::rules::types::{RuleEntry, RulesMap};
 use crate::rules::RuleBuilder;
 use std::collections::HashMap;
 
+
 #[macro_use]
 pub(crate) mod func_param_name_camelcase;
 pub(crate) mod contract_name_pascalcase;
 pub(crate) mod func_name_camelcase;
 pub(crate) mod use_forbidden_name;
+pub(crate) mod event_name_camelcase;
 
 // List all rules
 
@@ -21,6 +23,7 @@ pub fn create_default_rules() -> Vec<RuleEntry> {
         FuncNameCamelCase::create_default(),
         FuncParamNameCamelcase::create_default(),
         UseForbiddenName::create_default(),
+        EventNameCamelCase::create_default(),
     ]
 }
 
@@ -42,6 +45,10 @@ pub fn create_rules() -> RulesMap {
     rules.insert(
         use_forbidden_name::RULE_ID.to_string(),
         UseForbiddenName::create,
+    );
+    rules.insert(
+        event_name_camelcase::RULE_ID.to_string(),
+        EventNameCamelCase::create,
     );
 
     rules

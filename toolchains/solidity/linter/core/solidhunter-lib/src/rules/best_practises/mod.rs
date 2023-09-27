1d3a4363208bbb877cf412aa47357980db13a57d
leon3108 <maxime.noel@epitech.eu>
2023-09-27 11:01:47 +0100

    feat: push emptyBlock (test doesn't works)

--- toolchains/solidity/linter/core/solidhunter-lib/src/rules/best_practises/mod.rs
+++ toolchains/solidity/linter/core/solidhunter-lib/src/rules/best_practises/mod.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 pub mod line_maxlen;
 pub mod function_max_lines;
 pub mod max_states_count;
+pub mod no_empty_block;
 pub mod reason_string;
 
 // List all rules
@@ -12,6 +13,7 @@ pub mod reason_string;
 use crate::rules::best_practises::function_max_lines::FunctionMaxLines;
 use crate::rules::best_practises::line_maxlen::LineMaxLen;
 use crate::rules::best_practises::max_states_count::MaxStatesCount;
+use crate::rules::best_practises::no_empty_block::NoEmptyBlock;
 use crate::rules::best_practises::reason_string::ReasonString;
 use crate::rules::RuleBuilder;
 
@@ -21,6 +23,7 @@ pub fn create_default_rules() -> Vec<RuleEntry> {
         MaxStatesCount::create_default(),
         FunctionMaxLines::create_default(),
         ReasonString::create_default(),
+        NoEmptyBlock::create_default(),
     ]
 }
 
@@ -38,6 +41,7 @@ pub fn create_rules() -> RulesMap {
         FunctionMaxLines::create,
     );
     rules.insert(reason_string::RULE_ID.to_string(), ReasonString::create);
+    rules.insert(no_empty_block::RULE_ID.to_string(), NoEmptyBlock::create);
 
     rules
 }

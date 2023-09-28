33c3b872efb02e047f467c40a151732b9b547199
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2023-09-28 16:44:40 -0400

    feat(libs/ast-extractor): added enum values for some clickable types

--- libs/ast-extractor/src/retriever/finder/find_node.rs
+++ libs/ast-extractor/src/retriever/finder/find_node.rs
@@ -4,12 +4,36 @@
  * author: 0xMemoryGrinder
  */
 
-use syn_solidity::{ItemContract, ItemEnum};
+use syn_solidity::{ExprNew, ItemContract, ItemEnum, ItemFunction, VariableDeclaration, ExprCall, SolIdent, VariableDefinition, Type, VarDeclDecl, Expr};
 
 pub enum FoundNode {
-    // Contract Definition
-    ContractDef(ItemContract),
-    ContractRef(ItemContract),
+
+
+
+
+
+    // Contracts
+    ContractDefName(ItemContract),
+    ContractDefInheritance(ItemContract),
+    ContractInstantiation(ItemContract, Option<ItemFunction>, ExprNew),
+    //TODO : ContractScope(ItemContract, Option<ItemFunction>, ????),
+
+    // Functions
+    FunctionDefName(ItemContract, ItemFunction),
+    FunctionDefParameter(ItemContract, ItemFunction, VariableDeclaration),
+    FunctionDefReturn(ItemContract, ItemFunction, VariableDeclaration),
+    FunctionUsageName(ItemContract, ItemFunction, ExprCall),
+
+    // Properties
+    PropertyDefName(ItemContract, VariableDefinition, SolIdent),
+    PropertyDefType(ItemContract, VariableDefinition, Type),
+
+    // Variables
+    VariableDefName(ItemContract, VarDeclDecl, VariableDeclaration, Option<SolIdent>),
+    VariableDefType(ItemContract, VarDeclDecl, VariableDeclaration, Type),
+    VariableUsageName(Option<ItemContract>, Option<ItemFunction>, Expr, SolIdent),
+
+
 
 
 

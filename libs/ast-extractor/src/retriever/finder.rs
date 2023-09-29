79c1a6dbca91717999320e982a10148ef1ea8af8
0xSwapFeeder <tristan.gremont@epitech.eu>
2023-09-29 15:31:06 -0400

    wip(solidity/ast/finder): testing ...

--- libs/ast-extractor/src/retriever/finder.rs
+++ libs/ast-extractor/src/retriever/finder.rs
@@ -95,21 +95,36 @@ impl FinderVisitor {
 
 impl<'ast> Visit<'ast> for FinderVisitor {
     fn visit_stmt(&mut self, stmt: &'ast Stmt) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(stmt.span().start(), stmt.span().end(), self.to_find) {
             self.current_stmt = Some(stmt.clone());
+        }
+        if self.found.is_some() {
+            return;
         }
         visit::visit_stmt(self, stmt);
     }
 
     fn visit_expr(&mut self, expr: &'ast Expr) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(expr.span().start(), expr.span().end(), self.to_find) {
             self.current_expr = Some(expr.clone());
+        }
+        if self.found.is_some() {
+            return;
         }
         visit::visit_expr(self, expr);
     }
 
 
     fn visit_expr_call(&mut self, call: &'ast ExprCall) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(call.span().start(), call.span().end(), self.to_find) {
             self.current_expr = Some(Expr::Call(call.clone()));
             if !is_in_range!(call.args.span().start(), call.args.span().end(), self.to_find) {
@@ -117,27 +132,55 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                 return;
             }
         }
+        if self.found.is_some() {
+            return;
+        }
         visit::visit_expr_call(self, call);
     }
 
     //TODO: Found Limitation: cannot check parameter list of a new expr
     // Therefore we can not goto or list_ref any variable used in a new expr
     fn visit_expr_new(&mut self, new: &'ast ExprNew) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(new.span().start(), new.span().end(), self.to_find) {
             self.current_expr = Some(Expr::New(new.clone()));
             self.found = Some(FoundNode::ContractInstantiation(self.current_contract.clone().unwrap().clone(), self.current_function.clone(), new.clone()));
         }
+        if self.found.is_some() {
+            return;
+        }
         visit_expr_new(self, new);
     }
 
+    fn visit_ident(&mut self, ident: &'ast SolIdent) {
+        if self.found.is_some() {
+            return;
+        }
+        if is_in_range!(ident.span().start(), ident.span().end(), self.to_find) {
+            self.found = Some(FoundNode::VariableUsageName(self.current_contract.clone(), self.current_function.clone(), self.current_expr.clone().unwrap(), ident.clone()));
+            return;
+        }
+        if self.found.is_some() {
+            return;
+        }
+        visit::visit_ident(self, ident);
+    }
+
     fn visit_type(&mut self, ty: &'ast Type) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(ty.span().start(), ty.span().end(), self.to_find) {
             self.found = Some(FoundNode::TypeUsage(self.current_contract.clone(), self.current_function.clone(), self.current_expr.clone(), ty.clone()));
             return;
         }
+        if self.found.is_some() {
+            return;
+        }
         visit::visit_type(self, ty);
     }
-
     fn visit_variable_declaration(&mut self, var: &'ast VariableDeclaration) {
         if is_in_range!(var.span().start(), var.span().end(), self.to_find) {
             self.current_variable = Some(var.clone());
@@ -146,9 +189,17 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                     return;
             }
         }
+        if self.found.is_some() {
+            return;
+        }
         visit_variable_declaration(self, var);
+
     }
+
     fn visit_variable_definition(&mut self, var: &'ast VariableDefinition) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(var.span().start(), var.span().end(), self.to_find) {
             self.current_property = Some(var.clone());
             if is_in_range!(var.name.span().start(), var.name.span().end(), self.to_find) {
@@ -160,14 +211,18 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                 return;
             }
         }
+        if self.found.is_some() {
+            return;
+        }
         visit::visit_variable_definition(self, var);
-
     }
 
     fn visit_item_contract(&mut self, contract: &'ast ItemContract) {
-
+        if self.found.is_some() {
+            return;
+        }
+        self.current_contract = Some(contract.clone());
         if is_in_range!(contract.span().start(), contract.span().end(), self.to_find) {
-            self.current_contract = Some(contract.clone());
             if is_in_range!(contract.name.span().start(), contract.name.span().end(), self.to_find) {
                 self.found = Some(FoundNode::ContractDefName(contract.clone()));
                 return;
@@ -176,11 +231,16 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                 return;
             }
         }
+        if self.found.is_some() {
+            return;
+        }
         visit::visit_item_contract(self, contract);
-
     }
 
     fn visit_item_enum(&mut self, enumm: &'ast ItemEnum) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(enumm.span().start(), enumm.span().end(), self.to_find) {
             self.current_enum = Some(enumm.clone());
             if is_in_range!(enumm.name.span().start(), enumm.name.span().end(), self.to_find) {
@@ -195,10 +255,15 @@ impl<'ast> Visit<'ast> for FinderVisitor {
             }
         }
         visit::visit_item_enum(self, enumm);
-
+        if self.found.is_some() {
+            return;
+        }
     }
 
     fn visit_item_error(&mut self, error: &'ast ItemError) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(error.span().start(), error.span().end(), self.to_find) {
             self.current_error = Some(error.clone());
             if is_in_range!(error.name.span().start(), error.name.span().end(), self.to_find) {
@@ -213,10 +278,15 @@ impl<'ast> Visit<'ast> for FinderVisitor {
             }
         }
         visit::visit_item_error(self, error);
-
+        if self.found.is_some() {
+            return;
+        }
     }
 
     fn visit_item_event(&mut self, event: &'ast ItemEvent) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(event.span().start(), event.span().end(), self.to_find) {
             self.current_event = Some(event.clone());
             if is_in_range!(event.name.span().start(), event.name.span().end(), self.to_find) {
@@ -230,11 +300,16 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                 }
             }
         }
+        if self.found.is_some() {
+            return;
+        }
         visit::visit_item_event(self, event);
-
     }
 
     fn visit_item_function(&mut self, function: &'ast ItemFunction) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(function.span().start(), function.span().end(), self.to_find) {
             self.current_function = Some(function.clone());
             if is_in_range!(function.name.span().start(), function.name.span().end(), self.to_find) {
@@ -248,19 +323,16 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                 }
             }
         }
-        visit::visit_item_function(self, function);
-
-    }
-
-    fn visit_ident(&mut self, ident: &'ast SolIdent) {
-        if is_in_range!(ident.span().start(), ident.span().end(), self.to_find) {
-            self.found = Some(FoundNode::VariableUsageName(self.current_contract.clone(), self.current_function.clone(), self.current_expr.clone().unwrap(), ident.clone()));
+        if self.found.is_some() {
             return;
         }
-        visit::visit_ident(self, ident);
+        visit::visit_item_function(self, function);
     }
 
     fn visit_item_struct(&mut self, strukt: &'ast ItemStruct) {
+        if self.found.is_some() {
+            return;
+        }
         if is_in_range!(strukt.span().start(), strukt.span().end(), self.to_find) {
             self.current_struct = Some(strukt.clone());
             if is_in_range!(strukt.name.span().start(), strukt.name.span().end(), self.to_find) {
@@ -273,6 +345,9 @@ impl<'ast> Visit<'ast> for FinderVisitor {
                     return;
                 }
             }
+        }
+        if self.found.is_some() {
+            return;
         }
         visit::visit_item_struct(self, strukt);
     }
@@ -318,14 +393,52 @@ mod tests {
                     FoundNode::TypeUsage(_,_,_,ty) => {
                         match ty {
                             Type::String(_) => {assert!(true)}
-                            _ => {assert!(true)}
+                            _ => {assert!(false)}
                         }
                     }
-                    _ => {}
+                    _ => {
+                        assert!(false)
+                    }
                 }
 
             } else {
                 assert!(false)
             }
+    }
+
+    #[test]
+    fn test_retrieve_function_def_name() {
+        let source = String::from("pragma solidity ^0.8.0;\
+        abstract contract One {
+    uint storedData;
+    function set(uint x) public {
+        storedData = x;
+        string test2;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}");
+        let tokens = TokenStream::from_str(source.as_str()).unwrap();
+        let ast = parse2(tokens).unwrap();
+        let res = retrieve_node_from_position(&ast, Position::new(3, 14));
+        if let Some(node) = res {
+            match &node {
+                FoundNode::FunctionDefName(_,f) => {
+                    if let Some(name) = &f.name {
+                        assert_eq!(name.to_string(), "set");
+                    } else {
+                        assert!(false)
+                    }
+                }
+                _ => {
+                    assert!(false)
+                }
+            }
+
+        } else {
+            assert!(false)
+        }
     }
 }

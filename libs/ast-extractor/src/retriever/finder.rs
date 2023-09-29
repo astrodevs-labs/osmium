5e056471cde941e211277aea4e5c478a142e7959
0xSwapFeeder <tristan.gremont@epitech.eu>
2023-09-29 13:03:29 -0400

    feat(solidity/ast/finder): implemented core finder with found node enum (need tests!)

--- libs/ast-extractor/src/retriever/finder.rs
+++ libs/ast-extractor/src/retriever/finder.rs
@@ -7,7 +7,7 @@ use syn_solidity::*;
 use proc_macro2::LineColumn;
 use syn::ExprLit;
 use syn_solidity::kw::contract;
-use syn_solidity::visit::visit_variable_declaration;
+use syn_solidity::visit::{visit_expr_new, visit_variable_declaration};
 use crate::retriever::finder::find_node::FoundNode;
 
 mod find_node;
@@ -44,16 +44,16 @@ impl Default for Position {
 }
 
 struct FinderVisitor {
-    current_contract: Option<Box<ItemContract>>,
-    current_function: Option<Box<ItemFunction>>,
-    current_property: Option<Box<VariableDefinition>>,
-    current_variable: Option<Box<VariableDeclaration>>,
-    current_enum: Option<Box<ItemEnum>>,
-    current_struct: Option<Box<ItemStruct>>,
-    current_error: Option<Box<ItemError>>,
-    current_event: Option<Box<ItemEvent>>,
-    current_expr: Option<Box<Expr>>,
-    current_stmt: Option<Box<Stmt>>,
+    current_contract: Option<ItemContract>,
+    current_function: Option<ItemFunction>,
+    current_property: Option<VariableDefinition>,
+    current_variable: Option<VariableDeclaration>,
+    current_enum: Option<ItemEnum>,
+    current_struct: Option<ItemStruct>,
+    current_error: Option<ItemError>,
+    current_event: Option<ItemEvent>,
+    current_expr: Option<Expr>,
+    current_stmt: Option<Stmt>,
     found: Option<FoundNode>,
     to_find: Position,
 }
@@ -81,9 +81,9 @@ impl FinderVisitor {
     fn check_inheritance_matching(&mut self, contract: &ItemContract) -> bool {
         if let Some(inheritance) = &contract.inheritance {
             if is_in_range!(inheritance.span().start(), inheritance.span().end(), self.to_find) {
-                for inherit in inheritance.inheritance {
+                for inherit in &inheritance.inheritance {
                     if is_in_range!(inherit.span().start(), inherit.span().end(), self.to_find) {
-                        self.found = Some(FoundNode::ContractDefInheritance(contract.clone(), inherit));
+                        self.found = Some(FoundNode::ContractDefInheritance(contract.clone(), inherit.clone()));
                         return true;
                     }
                 }
@@ -91,86 +91,83 @@ impl FinderVisitor {
         }
         return false;
     }
-
-    pub fn check_matching(&mut self, node: Box<dyn Spanned>, node_type: String) -> bool {
-        let start_pos = node.span().start();
-        let end_pos = node.span().end();
-        if self._is_in_range(start_pos, end_pos) {
-            self.found = Some(FoundNode::new(
-                node_type,
-                node,
-                None
-            ));
-            return true;
-        }
-        return false;
-    }
-
-    pub fn check_matching_sub_span(&mut self, node: Box<dyn Spanned>, node_type: String, sub_span: Option<Box<dyn Spanned>>) -> bool{
-        let start_pos = sub_span.span().start();
-        let end_pos = sub_span.span().end();
-        if self._is_in_range(start_pos, end_pos) {
-            self.found = Some(FoundNode::new(
-                node_type,
-                node,
-                sub_span
-            ));
-            return true;
-        }
-        return false;
-    }
-
 }
 
 impl<'ast> Visit<'ast> for FinderVisitor {
-    fn visit_expr_call(&mut self, i: &'ast ExprCall) {
-        todo!()
+    fn visit_stmt(&mut self, stmt: &'ast Stmt) {
+        if is_in_range!(stmt.span().start(), stmt.span().end(), self.to_find) {
+            self.current_stmt = Some(stmt.clone());
+        }
+        visit::visit_stmt(self, stmt);
+    }
+
+    fn visit_expr(&mut self, expr: &'ast Expr) {
+        if is_in_range!(expr.span().start(), expr.span().end(), self.to_find) {
+            self.current_expr = Some(expr.clone());
+        }
+        visit::visit_expr(self, expr);
+    }
+
+
+    fn visit_expr_call(&mut self, call: &'ast ExprCall) {
+        if is_in_range!(call.span().start(), call.span().end(), self.to_find) {
+            self.current_expr = Some(Expr::Call(call.clone()));
+            if !is_in_range!(call.args.span().start(), call.args.span().end(), self.to_find) {
+                self.found = Some(FoundNode::FunctionUsageName(self.current_contract.clone().unwrap().clone(), self.current_function.clone().unwrap().clone(), call.clone()));
+                return;
+            }
+        }
+        visit::visit_expr_call(self, call);
+    }
+
+    //TODO: Found Limitation: cannot check parameter list of a new expr
+    // Therefore we can not goto or list_ref any variable used in a new expr
+    fn visit_expr_new(&mut self, new: &'ast ExprNew) {
+        if is_in_range!(new.span().start(), new.span().end(), self.to_find) {
+            self.current_expr = Some(Expr::New(new.clone()));
+            self.found = Some(FoundNode::ContractInstantiation(self.current_contract.clone().unwrap().clone(), self.current_function.clone(), new.clone()));
+        }
+        visit_expr_new(self, new);
+    }
+
+    fn visit_type(&mut self, ty: &'ast Type) {
+        if is_in_range!(ty.span().start(), ty.span().end(), self.to_find) {
+            self.found = Some(FoundNode::TypeUsage(self.current_contract.clone(), self.current_function.clone(), self.current_expr.clone(), ty.clone()));
+            return;
+        }
+        visit::visit_type(self, ty);
     }
 
     fn visit_variable_declaration(&mut self, var: &'ast VariableDeclaration) {
         if is_in_range!(var.span().start(), var.span().end(), self.to_find) {
-            self.current_variable = Some(Box::new(var.clone()));
+            self.current_variable = Some(var.clone());
             if is_in_range!(var.name.span().start(), var.name.span().end(), self.to_find) {
-                    if let VarDeclDecl();
-                    self.found = Some(FoundNode::VariableDefName(*self.current_contract.clone(), *self.current_function, var.clone()));
+                    self.found = Some(FoundNode::VariableDefName(self.current_contract.clone(), self.current_function.clone(), var.clone(), var.name.clone()));
                     return;
-                }
             }
-            visit_variable_declaration(self, var);
+        }
+        visit_variable_declaration(self, var);
     }
-
-    fn visit_parameter_list(&mut self, params: &'ast ParameterList) {
-        todo!()
-    }
-
-    fn visit_stmt_var_decl(&mut self, stmt_var_decl: &'ast StmtVarDecl) {
-        todo!("VariableDefName and variableDefType")
-    }
-
     fn visit_variable_definition(&mut self, var: &'ast VariableDefinition) {
         if is_in_range!(var.span().start(), var.span().end(), self.to_find) {
-            self.current_property = Some(Box::new(var.clone()));
-            if is_in_range!(var.ty.span().start(), var.ty.span().end(), self.to_find) {
-
-                self.found = Some(FoundNode::TypeUsage(*self.current_contract,self.current_function,None, *var.ty));
-                return;
-            }
+            self.current_property = Some(var.clone());
             if is_in_range!(var.name.span().start(), var.name.span().end(), self.to_find) {
                 if self.current_contract.is_none() {
-                    self.found = Some(FoundNode::ConstantVariableDefName(var.clone(), *var.name))
+                    self.found = Some(FoundNode::ConstantVariableDefName(var.clone(), var.name.clone()))
                 } else {
-                    self.found = Some(FoundNode::PropertyDefName(*self.current_contract,var.clone(), *var.name));
+                    self.found = Some(FoundNode::PropertyDefName(self.current_contract.clone().unwrap(),var.clone(), var.name.clone()));
                 }
                 return;
             }
-            visit::visit_variable_definition(self, var);
         }
+        visit::visit_variable_definition(self, var);
+
     }
 
     fn visit_item_contract(&mut self, contract: &'ast ItemContract) {
 
         if is_in_range!(contract.span().start(), contract.span().end(), self.to_find) {
-            self.current_contract = Some(Box::new(contract.clone()));
+            self.current_contract = Some(contract.clone());
             if is_in_range!(contract.name.span().start(), contract.name.span().end(), self.to_find) {
                 self.found = Some(FoundNode::ContractDefName(contract.clone()));
                 return;
@@ -178,54 +175,112 @@ impl<'ast> Visit<'ast> for FinderVisitor {
             if self.check_inheritance_matching(contract) {
                 return;
             }
-
-            visit::visit_item_contract(self, contract);
         }
+        visit::visit_item_contract(self, contract);
+
+    }
+
+    fn visit_item_enum(&mut self, enumm: &'ast ItemEnum) {
+        if is_in_range!(enumm.span().start(), enumm.span().end(), self.to_find) {
+            self.current_enum = Some(enumm.clone());
+            if is_in_range!(enumm.name.span().start(), enumm.name.span().end(), self.to_find) {
+                self.found = Some(FoundNode::EnumDefName(self.current_contract.clone(),enumm.clone(), enumm.name.clone()));
+                return;
+            }
+            for variant in &enumm.variants {
+                if is_in_range!(variant.ident.span().start(), variant.ident.span().end(), self.to_find) {
+                    self.found = Some(FoundNode::EnumDefValue(self.current_contract.clone(), enumm.clone(), variant.clone(), variant.ident.clone()));
+                    return;
+                }
+            }
+        }
+        visit::visit_item_enum(self, enumm);
+
+    }
+
+    fn visit_item_error(&mut self, error: &'ast ItemError) {
+        if is_in_range!(error.span().start(), error.span().end(), self.to_find) {
+            self.current_error = Some(error.clone());
+            if is_in_range!(error.name.span().start(), error.name.span().end(), self.to_find) {
+                self.found = Some(FoundNode::ErrorDefName(self.current_contract.clone(), error.clone(), error.name.clone()));
+                return;
+            }
+            for param in &error.parameters {
+                if is_in_range!(param.name.span().start(), param.name.span().end(), self.to_find) {
+                    self.found = Some(FoundNode::ErrorDefParameter(self.current_contract.clone(), error.clone(), param.clone()));
+                    return;
+                }
+            }
+        }
+        visit::visit_item_error(self, error);
+
+    }
+
+    fn visit_item_event(&mut self, event: &'ast ItemEvent) {
+        if is_in_range!(event.span().start(), event.span().end(), self.to_find) {
+            self.current_event = Some(event.clone());
+            if is_in_range!(event.name.span().start(), event.name.span().end(), self.to_find) {
+                self.found = Some(FoundNode::EventDefName(self.current_contract.clone().unwrap().clone(), event.clone(), event.name.clone()));
+                return;
+            }
+            for param in &event.parameters {
+                if is_in_range!(param.name.span().start(), param.name.span().end(), self.to_find) {
+                    self.found = Some(FoundNode::EventDefParameter(self.current_contract.clone().unwrap().clone(), event.clone(), param.clone()));
+                    return;
+                }
+            }
+        }
+        visit::visit_item_event(self, event);
+
     }
 
     fn visit_item_function(&mut self, function: &'ast ItemFunction) {
         if is_in_range!(function.span().start(), function.span().end(), self.to_find) {
-            self.current_function = Some(Box::new(function.clone()));
+            self.current_function = Some(function.clone());
             if is_in_range!(function.name.span().start(), function.name.span().end(), self.to_find) {
-                self.found = Some(FoundNode::FunctionDefName(*self.current_contract.clone(), function.clone()));
+                self.found = Some(FoundNode::FunctionDefName(self.current_contract.clone().unwrap(), function.clone()));
                 return;
-            }
-            if let Some(rturn) =  &function.returns {
-                rturn.returns.iter().for_each(|r| {
-                    if is_in_range!(r.ty.span().start(), r.ty.span().end(), self.to_find) {
-                        self.found = Some(FoundNode::TypeUsage(*self.current_contract, Some(function.clone()), None, r.ty.clone()));
-                        return;
-                    }
-                })
             }
             for param in &function.arguments {
                 if is_in_range!(param.name.span().start(), param.name.span().end(), self.to_find) {
-                    self.found = Some(FoundNode::FunctionDefParameterName(*self.current_contract, function.clone(), param.clone(), *param.name));
+                    self.found = Some(FoundNode::FunctionDefParameterName(self.current_contract.clone().unwrap(), function.clone(), param.clone(), param.name.clone()));
                     return;
                 }
             }
-            visit::visit_item_function(self, function);
         }
+        visit::visit_item_function(self, function);
+
     }
 
-    fn visit_stmt(&mut self, stmt: &'ast Stmt) {
-        self.current_stmt = Some(Box::new(stmt.clone()));
-        visit::visit_stmt(self, stmt);
+    fn visit_ident(&mut self, ident: &'ast SolIdent) {
+        if is_in_range!(ident.span().start(), ident.span().end(), self.to_find) {
+            self.found = Some(FoundNode::VariableUsageName(self.current_contract.clone(), self.current_function.clone(), self.current_expr.clone().unwrap(), ident.clone()));
+            return;
+        }
+        visit::visit_ident(self, ident);
     }
 
-    //TODO: Found Limitation: cannot check parameter list of a new expr
-    // Therefore we can not goto or list_ref any variable used in a new expr
-    fn visit_expr_new(&mut self, new: &'ast ExprNew) {
-        if is_in_range!(new.span().start(), new.span().end(), self.to_find) {
-            self.current_expr = Some(Box::new(Expr::New(new.clone())));
-            self.found = Some(FoundNode::ContractInstantiation(*self.current_contract.clone(), *self.current_function.clone(), new.clone()));
+    fn visit_item_struct(&mut self, strukt: &'ast ItemStruct) {
+        if is_in_range!(strukt.span().start(), strukt.span().end(), self.to_find) {
+            self.current_struct = Some(strukt.clone());
+            if is_in_range!(strukt.name.span().start(), strukt.name.span().end(), self.to_find) {
+                self.found = Some(FoundNode::StructDefName(self.current_contract.clone(), strukt.name.clone()));
+                return;
+            }
+            for field in &strukt.fields {
+                if is_in_range!(field.name.span().start(), field.name.span().end(), self.to_find) {
+                    self.found = Some(FoundNode::StructDefPropertyName(self.current_contract.clone().unwrap().clone(), self.current_function.clone(), field.clone(), field.name.clone()));
+                    return;
+                }
+            }
         }
+        visit::visit_item_struct(self, strukt);
     }
 
 }
 
 
-pub fn retrieve_node_from_position(ast: &syn_solidity::File, pos: Position) -> Option<find_node::FoundNode> {
+pub fn retrieve_node_from_position(ast: &File, pos: Position) -> Option<FoundNode> {
     let mut visitor = FinderVisitor::new(pos);
     visitor.visit_file(ast);
     visitor.found
@@ -258,17 +313,19 @@ mod tests {
         let tokens = TokenStream::from_str(source.as_str()).unwrap();
         let ast = syn_solidity::parse2(tokens).unwrap();
         let res = retrieve_node_from_position(&ast, Position::new(5, 8));
-        match &res {
-            Some(node) => {
-                match &node.sub_span {
-                    Some(span) => {assert!(true)}
+        if let Some(node) = res {
+                match &node {
+                    FoundNode::TypeUsage(_,_,_,ty) => {
+                        match ty {
+                            Type::String(_) => {assert!(true)}
+                            _ => {assert!(true)}
+                        }
+                    }
                     _ => {}
                 }
-                assert_eq!(node.node_type, "string");
 
+            } else {
+                assert!(false)
             }
-            _ => {assert!(false)}
-        }
-
     }
 }
--- libs/ast-extractor/src/retriever/finder/find_node.rs
+++ libs/ast-extractor/src/retriever/finder/find_node.rs
@@ -5,7 +5,7 @@ use syn::token::Struct;
  * author: 0xMemoryGrinder
  */
 
-use syn_solidity::{ExprNew, ItemContract, ItemEnum, ItemFunction, VariableDeclaration, ExprCall, SolIdent, VariableDefinition, Type, VarDeclDecl, Expr, SolPath, File, Variant, Item, ItemEvent, ItemError, Modifier};
+use syn_solidity::{ExprNew, ItemContract, ItemEnum, ItemFunction, VariableDeclaration, ExprCall, SolIdent, VariableDefinition, Type, VarDeclDecl, Expr, SolPath, File, Variant, Item, ItemEvent, ItemError, Modifier, EventParameter};
 
 pub enum FoundNode {
     // Contracts
@@ -16,60 +16,61 @@ pub enum FoundNode {
 
     // Functions
     FunctionDefName(ItemContract, ItemFunction),
-    FunctionDefParameterName(ItemContract, ItemFunction, VariableDeclaration, SolIdent),
-
-    // Properties
-    PropertyDefName(ItemContract, VariableDefinition, SolIdent),
-    //PropertyDefType(ItemContract, VariableDefinition, Type),
-    ConstantVariableDefName(VariableDefinition, SolIdent),
-
-
-
-
-
-
-
-
-
-    //ContractScope(ItemContract, Option<ItemFunction>, SolPath),
-
-    //FunctionDefParameterType(ItemContract, ItemFunction, VariableDeclaration, Type),
-
+    FunctionDefParameterName(ItemContract, ItemFunction, VariableDeclaration, Option<SolIdent>),
     FunctionUsageName(ItemContract, ItemFunction, ExprCall),
 
-    // Properties
-
-    // Variables
-    VariableDefName(Option<ItemContract>, Option<ItemFunction> VarDeclDecl, VariableDeclaration, Option<SolIdent>),
-    VariableDefType(File,ItemContract, VarDeclDecl, VariableDeclaration, Type),
-    VariableUsageName(Option<ItemContract>, Option<ItemFunction>, Expr, SolIdent),
-
-    // Enum
-    EnumDefName(File,Option<ItemContract>, ItemEnum, SolIdent),
-    EnumDefValue(Option<ItemContract>, ItemEnum, Variant, SolIdent),
-
+    // Properties/Variables/Constants
+    PropertyDefName(ItemContract, VariableDefinition, SolIdent),
+    ConstantVariableDefName(VariableDefinition, SolIdent),
+    VariableDefName(Option<ItemContract>, Option<ItemFunction>, VariableDeclaration, Option<SolIdent>),
+    TypeUsage(Option<ItemContract>, Option<ItemFunction>, Option<Expr>, Type),
 
     // Structs
     StructDefName(Option<ItemContract>, SolIdent),
-    StructInstantiation(ItemContract, Option<ItemFunction>, SolIdent),
-    StructDefPropertyName(ItemContract, Option<ItemFunction>, VariableDeclaration, SolIdent),
-    //StructUsageName(ItemContract, Option<ItemFunction>, Expr, Type),
+    StructDefPropertyName(ItemContract, Option<ItemFunction>, VariableDeclaration, Option<SolIdent>),
 
-    //StructUsageProperty(ItemContract, Option<ItemFunction>, Expr, SolIdent, Type),
 
+    // Enum
+    EnumDefName(Option<ItemContract>, ItemEnum, SolIdent),
+    EnumDefValue(Option<ItemContract>, ItemEnum, Variant, SolIdent),
 
     // Errors
     ErrorDefName(Option<ItemContract>, ItemError, SolIdent),
     ErrorDefParameter(Option<ItemContract>, ItemError, VariableDeclaration),
+
+    // Events
+    EventDefName(ItemContract, ItemEvent, SolIdent),
+    EventDefParameter(ItemContract, ItemEvent, EventParameter),
+
+    /// TODO!:
+    /// HOW TO DIFFERENTIATE BETWEEN THESE TWO?
+    VariableUsageName(Option<ItemContract>, Option<ItemFunction>, Expr, SolIdent),
+    StructInstantiation(ItemContract, Option<ItemFunction>, SolIdent),
+
+    // Contracts
+    //ContractScope(ItemContract, Option<ItemFunction>, SolPath),
+
+    // Functions
+    //FunctionDefParameterType(ItemContract, ItemFunction, VariableDeclaration, Type),
+
+    // Properties
+    //PropertyDefType(ItemContract, VariableDefinition, Type),
+
+    // Variables
+    //VariableDefType(File,ItemContract, VarDeclDecl, VariableDeclaration, Type),
+
+    // Structs
+    //StructUsageName(ItemContract, Option<ItemFunction>, Expr, Type),
+    //StructUsageProperty(ItemContract, Option<ItemFunction>, Expr, SolIdent, Type),
+
+
+    // Errors
     //ErrorUsageName(ItemContract, Option<ItemFunction>, ExprCall, SolIdent),
 
 
     // Events
-    EventDefName(ItemContract, ItemEvent, SolIdent),
-    EventDefParameter(ItemContract, ItemEvent, VariableDeclaration),
     //EventUsageName(ItemContract, Option<ItemFunction>, Expr, SolIdent),
 
-    TypeUsage(Option<ItemContract>, Option<ItemFunction>, Option<Expr>, Type),
 
     //TODO type cast
     // TODO super ast node

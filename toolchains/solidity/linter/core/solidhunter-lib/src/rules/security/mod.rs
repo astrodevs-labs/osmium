d5e0a9b106ab8ba861d73f1996ff54c1e03d52ba
Théo Maringer <theo.maringer@epitech.eu>
2023-10-03 18:20:16 +0100

    feat(solidity/linter/core): StateVisibility rule

--- toolchains/solidity/linter/core/solidhunter-lib/src/rules/security/mod.rs
+++ toolchains/solidity/linter/core/solidhunter-lib/src/rules/security/mod.rs
@@ -3,13 +3,15 @@ use std::collections::HashMap;
 
 #[macro_use]
 pub(crate) mod no_inline_assembly;
+pub(crate) mod state_visibility;
 
 // List all rules
 use crate::rules::security::no_inline_assembly::NoInlineAssembly;
+use crate::rules::security::state_visibility::StateVisibility;
 use crate::rules::RuleBuilder;
 
 pub fn create_default_rules() -> Vec<RuleEntry> {
-    vec![NoInlineAssembly::create_default()]
+    vec![NoInlineAssembly::create_default(), StateVisibility::create_default()]
 }
 
 pub fn create_rules() -> RulesMap {
@@ -18,6 +20,11 @@ pub fn create_rules() -> RulesMap {
     rules.insert(
         no_inline_assembly::RULE_ID.to_string(),
         NoInlineAssembly::create,
+    );
+
+    rules.insert(
+        state_visibility::RULE_ID.to_string(),
+        StateVisibility::create,
     );
 
     rules

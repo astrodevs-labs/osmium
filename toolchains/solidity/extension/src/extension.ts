3e0c5cfae1caf7fc5a757018c92d98a61e324a3a
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2023-12-03 16:40:36 -0500

    fix(solidity/extension): refactored extension to launch platform specific servers executable

--- toolchains/solidity/extension/src/extension.ts
+++ toolchains/solidity/extension/src/extension.ts
@@ -13,53 +13,18 @@ import {
 	ServerOptions,
 	TransportKind
 } from 'vscode-languageclient/node';
+import { createLinterClient } from './linter';
 
-let client: LanguageClient;
+
+let linterClient: LanguageClient;
 
 export async function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	const serverBinary = context.asAbsolutePath(
-		path.join('dist', 'linter-server')
-	);
+	linterClient = createLinterClient(context);
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { command: serverBinary, transport: TransportKind.stdio },
-		debug: {
-			command: serverBinary,
-			transport: TransportKind.stdio,
-		}
-	};
-
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'solidity' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.solidhunter.json')
-		}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'osmium-solidity',
-		'Osmium Solidity Language Server',
-		serverOptions,
-		clientOptions
-	);
-
-	client.onRequest('osmium/getContent', async (params) => {
-    	const contentUint8 = await workspace.fs.readFile(Uri.parse(params.uri));
-    	const content = new TextDecoder().decode(contentUint8);
-    	return {
-			content,
-		};
-	});
-
-	// Start the client. This will also launch the server
-	client.start();
+	// Push the disposable to the context's subscriptions so that the
+	// client can be deactivated on extension deactivation
+	context.subscriptions.push(linterClient);
+	
 
 	const folders = workspace.workspaceFolders;
 	if (folders) {
@@ -69,11 +34,4 @@ export async function activate(context: ExtensionContext) {
 		});
 	}
 
-}
-
-export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
 }

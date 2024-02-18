8824548fb609db1b9de2b02a40943f70a7ca6e61
0xtekgrinder <0xtekgrinder@protonmail.com>
2024-02-18 10:41:09 -0500

    feat: add tests and forge inspect gas estimation fully working

--- toolchains/solidity/extension/src/gas-estimation.ts
+++ toolchains/solidity/extension/src/gas-estimation.ts
@@ -1,4 +1,4 @@
-import { exec } from "child_process";
+import { execSync, exec } from "child_process";
 import * as vscode from "vscode";
 
 type GasReport = {
@@ -15,62 +15,115 @@ type Function = {
 
 type Report = Map<string, Map<string, GasReport>>;
 
+type ReportDecorators = Map<string, vscode.DecorationOptions[]>;
+
 function isForgeInstalled(): boolean {
   try {
-    exec("forge -version", (error: any, _stdout: any, _stderr: any) => {
-      if (error) {
-        throw error;
-      }
-    });
-    console.log("Forge is installed");
+    execSync("forge --version");
     return true;
   } catch (error) {
     return false;
   }
 }
 
-// Contracts needs to be formatted like this : ["contract1.sol:Contract1", "contract2.sol:Contract2"]
-function getGasReport(contracts: string[], cwd: string): Report {
-  const report: Report = new Map();
+async function gasReportTests(cwd: string): Promise<ReportDecorators> {
+  const reports: Map<string, Report> = new Map();
+  let decorations: ReportDecorators = new Map();
+
+  if (!isForgeInstalled()) {
+    console.log("Forge is not installed");
+    return decorations;
+  }
+
 
   // Gas estimation from the tests
-  exec("forge test --gas-report", { cwd }, (error: any, _stdout: any, _stderr: any) => {
+  await new Promise<void>((resolve, reject) => exec("forge test --gas-report", { cwd }, async (error: any, _stdout: any, _stderr: any) => {
     if (error) {
-      throw error;
+      console.log("error", error);
+      reject(error);
     }
 
+     if (_stdout === "null") {
+        resolve();
+      }
+
     let contractName = "";
-    _stdout.split("\n").forEach((line: string) => {
+    await Promise.all(_stdout.split("\n").map(async (line: string) => {
       const lineParts = line.split("|");
-      if (lineParts.length === 6) {
+      if (lineParts.length === 8) {
         const trimmedLineParts = lineParts.map((part) => part.trim());
-        if (trimmedLineParts[0] !== "" && trimmedLineParts[1] === "" && trimmedLineParts[2] === "" && trimmedLineParts[3] === "" && trimmedLineParts[4] === "" && trimmedLineParts[5] === "") {
-          contractName = trimmedLineParts[0];
-          return;
+        if (trimmedLineParts[1] !== "" && trimmedLineParts[2] === "" && trimmedLineParts[3] === "" && trimmedLineParts[4] === "" && trimmedLineParts[5] === "" && trimmedLineParts[6] === "") {
+          contractName = trimmedLineParts[1].split(" ")[0];
         }
 
-        if (trimmedLineParts[0] !== "" && trimmedLineParts[1] !== "" && trimmedLineParts[2] !== "" && trimmedLineParts[3] !== "" && trimmedLineParts[4] !== "" && trimmedLineParts[5] !== ""
-          && lineParts[0].replace("-", "") !== "" && lineParts[1].replace("-", "") !== "" && lineParts[2].replace("-", "") !== "" && lineParts[3].replace("-", "") !== "" && lineParts[4].replace("-", "") !== "" && lineParts[5].replace("-", "") !== ""
-          && trimmedLineParts[0] !== "Function Name") {
+        if (trimmedLineParts[1] !== "" && trimmedLineParts[2] !== "" && trimmedLineParts[3] !== "" && trimmedLineParts[4] !== "" && trimmedLineParts[5] !== "" && trimmedLineParts[6] !== ""
+          && !trimmedLineParts[1].split("").every(char => char === '-') && !trimmedLineParts[2].split("").every(char => char === '-') && !trimmedLineParts[3].split("").every(char => char === '-') && !trimmedLineParts[4].split("").every(char => char === '-') && !trimmedLineParts[5].split("").every(char => char === '-') && !trimmedLineParts[6].split("").every(char => char === '-')
+          && trimmedLineParts[1] !== "Function Name") {
+
           const functionName = trimmedLineParts[1];
           const min = BigInt(trimmedLineParts[2]);
           const average = BigInt(trimmedLineParts[3]);
           const median = BigInt(trimmedLineParts[4]);
           const max = BigInt(trimmedLineParts[5]);
 
-          report.set(contractName, new Map());
-          report.get(contractName)?.set(functionName, { min, average, median, max });
+          const splittedContractName = contractName.split(":");
+          const totalPath = `${cwd}/${splittedContractName[0]}`;
+          if (!reports.has(totalPath)) {
+            reports.set(totalPath, new Map());
+          }
+          if (reports.get(totalPath)?.has(splittedContractName[1])) {
+            reports.get(totalPath)?.get(splittedContractName[1])?.set(functionName, { min, average, median, max });
+          } else {
+            reports.get(totalPath)?.set(splittedContractName[1], new Map());
+            reports.get(totalPath)?.get(splittedContractName[1])?.set(functionName, { min, average, median, max });
+          }
         }
       }
-    });
-  });
+    }));
+    resolve();
+  }));
+
+  for (const [path, report] of reports) {
+    let decorationsArray: vscode.DecorationOptions[] = [];
+    const content = await vscode.workspace.fs.readFile(vscode.Uri.file(path));
+
+    for (const [contract, _] of report) {
+      const functionsInsideContract = getFunctionsInsideContract(content.toString(), contract);
+      for (const func of functionsInsideContract) {
+        const gas = report.get(contract)?.get(func.name)?.average;
+        if (!gas) {
+          continue;
+        }
+
+        let range = new vscode.Range(
+          new vscode.Position(func.line - 1, 0),
+          new vscode.Position(func.line - 1, content.toString().split("\n")[func.line - 1].length)
+        );
+        let decoration = { range, renderOptions: { after: { contentText: `    ${gas.toString()} gas` } }  };
+        decorationsArray.push(decoration);
+      }
+    }
+    decorations.set(path, decorationsArray);
+  }
+
+  return decorations;
+}
+
+// Contracts needs to be formatted like this : ["contract1.sol:Contract1", "contract2.sol:Contract2"]
+async function getGasReport(contracts: string[], cwd: string): Promise<Report> {
+  const report: Report = new Map();
 
   // Gas estimation from the contracts inspection
-  contracts.forEach((contract) => {
-    // TODO find config path
-    exec(`forge inspect ${contract} gasEstimates`, { cwd }, (error: any, _stdout: any, _stderr: any) => {
+  await Promise.all(contracts.map(async (contract) => {
+    return await new Promise<void>((resolve, reject) => {
+      exec(`forge inspect "${contract}" gasEstimates`, { cwd }, (error: any, _stdout: any, _stderr: any) => {
       if (error) {
-        throw error;
+        console.log("error", error);
+        reject(error);
+      }
+
+      if (_stdout === "null") {
+        resolve();
       }
 
       const json = JSON.parse(_stdout);
@@ -78,29 +131,33 @@ function getGasReport(contracts: string[], cwd: string): Report {
       const externalFunctions = Object.keys(json.external);
 
       internalFunctions.forEach((functionName) => {
+        const cleanFunctionName = functionName.split("(")[0];
         const res: string = json.internal[functionName];
         if (res !== "infinite") {
           if (report.has(contract)) {
-            report.get(contract)?.set(functionName, { average: BigInt(res) });
+            report.get(contract)?.set(cleanFunctionName, { average: BigInt(res) });
           } else {
             report.set(contract, new Map());
-            report.get(contract)?.set(functionName, { average: BigInt(res) });
+            report.get(contract)?.set(cleanFunctionName, { average: BigInt(res) });
           }
         }
       });
       externalFunctions.forEach((functionName) => {
+        const cleanFunctionName = functionName.split("(")[0];
         const res: string = json.external[functionName];
         if (res !== "infinite") {
           if (report.has(contract)) {
-            report.get(contract)?.set(functionName, { average: BigInt(res) });
+            report.get(contract)?.set(cleanFunctionName, { average: BigInt(res) });
           } else {
             report.set(contract, new Map());
-            report.get(contract)?.set(functionName, { average: BigInt(res) });
+            report.get(contract)?.set(cleanFunctionName, { average: BigInt(res) });
           }
         }
       });
+      resolve();
     });
   });
+  }));
   return report;
 }
 
@@ -126,6 +183,9 @@ function getContractsInsideFile(content: string, path: string): string[] {
   lines.forEach((line) => {
     if (getXthWord(line, 0) === "contract") {
       const contractName = getXthWord(line, 1);
+      contracts.push(`${path}:${contractName}`);
+    } else if (getXthWord(line, 0) === "abstract" && getXthWord(line, 1) === "contract") {
+      const contractName = getXthWord(line, 2);
       contracts.push(`${path}:${contractName}`);
     }
   });
@@ -166,59 +226,119 @@ function getFunctionsInsideContract(content: string, contractName: string): Func
   return functions;
 }
 
-function gasReport(content: string, path: string, editor: vscode.TextEditor, decorationType: vscode.TextEditorDecorationType) {
+async function gasReport(content: string, path: string): Promise<vscode.DecorationOptions[]> {
   if (!isForgeInstalled()) {
-    return;
+    console.log("Forge is not installed");
+    return [];
   }
   const workspace = vscode.workspace.workspaceFolders?.[0];
   const workspacePath = workspace?.uri.path;
   if (!workspacePath) {
-    return;
+    return [];
   }
   const contracts = getContractsInsideFile(content, path);
-  const report = getGasReport(contracts, workspacePath);
+  const report = await getGasReport(contracts, workspacePath);
+
   const functionsPerContract: Map<string, Function[]> = new Map();
   contracts.map((contract) => {
     const functions = getFunctionsInsideContract(content, contract.split(":")[1]);
-    functionsPerContract.set(contract.split(":")[0], functions);
+    functionsPerContract.set(contract, functions);
   });
 
-  let decorationsArray: vscode.DecorationOptions[] = []
+  let decorationsArray: vscode.DecorationOptions[] = [];
   for (const [contract, functions] of functionsPerContract) {
     for (const func of functions) {
-      // TODO: display the report close to the functions definitions
       const gas = report.get(contract)?.get(func.name)?.average;
-
       if (!gas) {
         continue;
       }
 
       let range = new vscode.Range(
-        new vscode.Position(func.line, 0),
-        new vscode.Position(func.line, content.split("\n")[func.line].length)
-      )
+        new vscode.Position(func.line - 1, 0),
+        new vscode.Position(func.line - 1, content.split("\n")[func.line - 1].length)
+      );
 
-      let decoration = { range, renderOptions: { after: { contentText: gas.toString() } }  }
-      decorationsArray.push(decoration)
+      let decoration = { range, renderOptions: { after: { contentText: `    ${gas.toString()} gas` } }  };
+      decorationsArray.push(decoration);
     }
   }
-  editor.setDecorations(decorationType, decorationsArray)
+  return decorationsArray;
+}
+
+async function showReport(editor: vscode.TextEditor, reports: ReportDecorators, reportsSaved: ReportDecorators, decorationType: vscode.TextEditorDecorationType) {
+  let report = reports.get(editor.document.uri.path);
+    const reportSaved = reportsSaved.get(editor.document.uri.path);
+
+    if (!report || !reportSaved) {
+    } else if (report && !reportSaved) {
+      editor.setDecorations(decorationType, report);
+    } else if (!report && reportSaved) {
+      editor.setDecorations(decorationType, reportSaved);
+    } else {
+      report = report.filter((reportElement) => {
+        return !reportSaved.some((reportSavedElement) => {
+          return reportElement.range.isEqual(reportSavedElement.range);
+        });
+      });
+      const combinedReport = [...(report || []), ...(reportSaved || [])];
+      editor.setDecorations(decorationType, combinedReport);
+    }
 }
 
 export function registerGasEstimation() {
   const decorationType = vscode.window.createTextEditorDecorationType({
     after: {
-      opacity: "0.5"
+      color: 'rgba(255, 255, 255, 0.5)'
     }
   });
 
-  vscode.workspace.onDidOpenTextDocument((document) => {
+  const reports: ReportDecorators  = new Map();
+  let reportsSaved: ReportDecorators  = new Map();
+
+  vscode.workspace.onDidOpenTextDocument(async (document) => {
     // gas estimate only the main contracts
-    if (!document.fileName.includes("lib") && !document.fileName.includes("test"))
-      gasReport(document.getText(), document.uri.path, vscode.window.activeTextEditor, decorationType);
+    if (document.uri.path.includes("lib") || document.uri.path.includes("test") || document.uri.path.includes("script") || document.uri.path.includes(".git")) {
+      return;
+    }
+    const report = await gasReport(document.getText(), document.uri.path);
+    reports.set(document.uri.path, report);
+
+    vscode.window.visibleTextEditors.forEach((editor) => {
+      showReport(editor, reports, reportsSaved, decorationType);
+    });
   });
 
-  vscode.workspace.onDidSaveTextDocument((document) => {
-    gasReport(document.getText(), document.uri.path, vscode.window.activeTextEditor, decorationType);
+  vscode.workspace.onDidSaveTextDocument(async (document) => {
+    if (document.uri.path.includes("lib") || document.uri.path.includes("test") || document.uri.path.includes("script") || document.uri.path.includes(".git")) {
+      return;
+    }
+    const report = await gasReport(document.getText(), document.uri.path);
+    reports.set(document.uri.path, report);
+
+    vscode.window.visibleTextEditors.forEach((editor) => {
+      showReport(editor, reports, reportsSaved, decorationType);
+    });
   });
+
+  vscode.window.onDidChangeVisibleTextEditors(async (editors) => {
+    editors.forEach((editor) => {
+      showReport(editor, reports, reportsSaved, decorationType);
+    });
+  });
+
+  vscode.window.onDidChangeActiveTextEditor(async (editor) => {
+    if (editor) {
+      showReport(editor, reports, reportsSaved, decorationType);
+    }
+  });
+
+  vscode.commands.registerCommand(
+    "osmium.gas-estimation",
+    async function () {
+      if (vscode.workspace.workspaceFolders?.[0].uri.fsPath) {
+        const report = await gasReportTests(vscode.workspace.workspaceFolders?.[0].uri.fsPath);
+        reportsSaved = report;
+      }
+    }
+  );
 }
